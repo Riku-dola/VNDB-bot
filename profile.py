3b034ea92e42873d566bb1736ea8203eb449028a
blankaex <blankaex@protonmail.com>
2019-09-10 11:21:02 +1000

    Changed nickname field to username

--- profile.py
+++ profile.py
@@ -5,14 +5,13 @@ client = discord.Client()
 @client.event
 async def on_ready():
     choice = random.choice(glob.glob('avatars/*'))
-    nick = os.path.basename(choice).split('.', 1)[0]
+    username = os.path.basename(choice).split('.', 1)[0]
     with open(choice, 'rb') as file:
         avatar = file.read()
     try:
-        await client.user.edit(nick=nick, avatar=avatar)
+        await client.user.edit(username=username, avatar=avatar)
     except:
         pass
-    await client.logout()
 
 with open('tokens/discord', 'r') as token:
     client.run(token.read().strip())
